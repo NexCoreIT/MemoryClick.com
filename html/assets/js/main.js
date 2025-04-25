c29cd5ebb840420b5194c30ec206dc259fafa38f
rabinmia7578 <rabinmia7578@gmail.com>
2025-04-25 12:02:25 +0600

    d

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -14,22 +14,22 @@
         autoplay: false,
         smartSpeed: 1000,
         margin: 25,
-        loop: false, 
+        loop: false,
         dots: false,
         nav: true,
-        navText : [
+        navText: [
             '<i class="bi bi-chevron-left"></i>',
             '<i class="bi bi-chevron-right"></i>'
         ],
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             },
-            768:{
-                items:2
+            768: {
+                items: 2
             },
-            992:{
-                items:3
+            992: {
+                items: 3
             }
         }
     });
@@ -39,26 +39,73 @@
         autoplay: false,
         smartSpeed: 1000,
         margin: 25,
-        loop: false, 
+        loop: false,
         dots: false,
         nav: true,
-        navText : [
+        navText: [
             '<i class="bi bi-chevron-left"></i>',
             '<i class="bi bi-chevron-right"></i>'
         ],
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             },
-            768:{
-                items:2
+            768: {
+                items: 2
             },
-            992:{
-                items:3
+            992: {
+                items: 3
             }
         }
     });
 
-    
+    //  cinematography carousel 
+    $(".cinematography-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1000,
+        margin: 25,
+        loop: false,
+        dots: false,
+        nav: true,
+        navText: [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
+            1400: {
+                items: 4
+            }
+        }
+    });
+
+
+    $(".filter-button").click(function () {
+        var value = $(this).attr('data-filter');
+
+        // Filter logic
+        if (value == "all") {
+            $('.filter').show('1000');
+        } else {
+            $(".filter").not('.' + value).hide('3000');
+            $('.filter').filter('.' + value).show('3000');
+        }
+
+        // Active class toggle
+        $(".filter-button").removeClass("active"); // Remove from all
+        $(this).addClass("active");                // Add to clicked button
+    });
+
+
+
+
 })(jQuery);
 
