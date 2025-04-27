49ff0372241ab2210330d542e204cdfb8d172256
rabinmia7578 <rabinmia7578@gmail.com>
2025-04-27 20:21:20 +0600

    d

--- resources/views/frontend/components/fixed/hero.blade.php
+++ resources/views/frontend/components/fixed/hero.blade.php
@@ -16,6 +16,12 @@
                 </div>
             @endforeach
         </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left-icon lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right-icon lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>   
+          </button>
     </div>
 </div>
 <!-- Header End -->
